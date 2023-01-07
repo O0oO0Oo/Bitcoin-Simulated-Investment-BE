6b9a37b1a68ebd14997f9566dec87f7c1f90f44b
BA <windy01999@gmail.com>
2023-01-07 12:32:15 +0900

    #4 - Quart
    Quart 설정, 1초마다 Json 데이터를 받아오는 Job 생성
    application.yaml : in-memory 방식이 아닌 jdbc에 JobStore 정보 저장
    TODO: PeriodicGetJsonDataTask 한번에 ObjectMapper를 이용해서 저장할 수 있도록 방법 찾기

---
+++ src/main/java/com/cryptocurrency/investment/scheduler/quartz/PeriodicGetJsonDataTask.java
@@ -0,0 +1,62 @@
+package com.cryptocurrency.investment.scheduler.quartz;
+
+import com.cryptocurrency.investment.Service.redis.CurrencyPriceRedisService;
+import com.cryptocurrency.investment.domain.redis.CurrencyPriceRedis;
+import com.cryptocurrency.investment.domain.redis.request.CryptocurrencyJson;
+import com.cryptocurrency.investment.domain.redis.request.CryptocurrencyJsonPriceData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PeriodicGetJsonDataTask implements Job {
+
+    @Autowired
+    CurrencyPriceRedisService redisService;
+
+    /**
+     * Todo: 현재는 request 패키지의 Class들에 ObjectMapper를 통해 매핑 후 다시 CurrencyPriceRedis 생성자를 통해 저장하고 있다.
+     * Todo: 한 단계 거치지 않고 바로 redis에 저장할수있는 매핑방법을 찾아보기
+     */
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        URL url;
+        HttpURLConnection conn;
+        InputStream responseBody;
+        ObjectMapper mapper = new ObjectMapper();
+        CryptocurrencyJson readValue;
+        try {
+            url = new URL("https://api.bithumb.com/public/ticker/ALL");
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            responseBody = conn.getInputStream();
+            readValue = mapper.readValue(responseBody, CryptocurrencyJson.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        CryptocurrencyJson.CryptocurrencyJsonInnerInfo innerInfo = readValue.getCryptocurrencyJsonInnerInfo();
+        HashMap<String, CryptocurrencyJsonPriceData> fields = readValue.getCryptocurrencyJsonInnerInfo().getFields();
+        LocalDateTime localDateTime = readValue.getCryptocurrencyJsonInnerInfo().getTimestamp();
+
+        for (Map.Entry<String, CryptocurrencyJsonPriceData> entry : fields.entrySet()) {
+            CurrencyPriceRedis priceRedis = new CurrencyPriceRedis(
+                    readValue.getCryptocurrencyJsonInnerInfo().getTimestamp() + entry.getKey(),
+                    entry.getKey(),localDateTime,
+                    entry.getValue().getClosing_price());
+            redisService.savePriceInfo(priceRedis);
+        }
+    }
+}
