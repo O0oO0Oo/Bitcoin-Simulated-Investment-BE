00defe79e4d4520e992ca455e2a698472bb4168a
BA <windy01999@gmail.com>
2023-01-19 21:46:36 +0900

    #10 user save
    유저 세이브 기능 추가

--- src/main/java/com/cryptocurrency/investment/user/controller/UserController.java
+++ src/main/java/com/cryptocurrency/investment/user/controller/UserController.java
@@ -1,29 +1,34 @@
 package com.cryptocurrency.investment.user.controller;
 
 
-import com.cryptocurrency.investment.user.validation.SaveCheck;
+import com.cryptocurrency.investment.user.dto.UserSignUpDto;
+import com.cryptocurrency.investment.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
-    @GetMapping("")
-    public String loginPage() {
-        return "login";
+    private final UserService userService;
+
+
+    /**
+     * @param userJoinDto Request 유저 Dto
+     * @return 성공시 가입시 기입한 username 반환
+     */
+    @PostMapping
+    public @ResponseBody String join(@RequestBody @Validated UserSignUpDto userJoinDto) {
+        return userService.userSave(userJoinDto.username(), userJoinDto.email(),userJoinDto.password());
     }
 
-
-    @GetMapping("/{id}")
-    public String addUser(@Validated(SaveCheck.class) @PathVariable String id) {
-
-        return "ok";
+    @GetMapping("/attendance")
+    public @ResponseBody String attendance(){
+        return LocalDateTime.now().toString();
     }
 }
