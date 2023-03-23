b32e397df2401d59ed4fe843492eb2b2bf6ac963
BA <windy01999@gmail.com>
2023-03-24 02:49:55 +0900

    #24-Todo-solved
    JsonIgnoreProperties 를 사용하여 사용하지 않는 데이터 저장 않하도록 개선

--- src/main/java/com/cryptocurrency/investment/price/dto/request/RequestPriceInfoDto.java
+++ src/main/java/com/cryptocurrency/investment/price/dto/request/RequestPriceInfoDto.java
@@ -5,27 +5,26 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Data
 public class RequestPriceInfoDto {
     @JsonProperty("status")
     private String status;
-
     @JsonProperty("data")
     private InnerData innerData;
-
-    private HashMap<String, RequestPriceInfoInnerDto> fields = new HashMap<>();
-    public class InnerData{
+    /**
+     * TODO : ConcurrentHashMap 으로 시간들이 뒤섞여서 저장되는것은 막았지만 아직 시간이 뛰엄뛰엄 저장됨 -> 메모리 문제인지 확인
+     */
+    private ConcurrentHashMap<String, RequestPriceInfoInnerDto> fields = new ConcurrentHashMap<>();
+    @Data
+    public class InnerData {
         @JsonProperty("date")
         private Long timestamp;
         @JsonAnySetter
-        public void setFiled(String field, RequestPriceInfoInnerDto value) {
+        public void setField(String field, RequestPriceInfoInnerDto value) {
             fields.put(field, value);
-        }
-
-        public void setTimestamp(Long timestamp) {this.timestamp = timestamp;}
-        public Long getTimestamp() {
-            return timestamp;
         }
     }
 }
+
